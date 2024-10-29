fba7b4e05b8d5b88872c8080582d75da84198db5
Jonathan-Ultini <jonathanultini@gmail.com>
2024-10-29 16:36:27 +0100

    set all data

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pokemon;
 use Illuminate\Http\Request;
 
 class PokemonController extends Controller
@@ -11,32 +12,54 @@ class PokemonController extends Controller
      */
     public function index()
     {
-        //
+        $pokemons = Pokemon::all();
+        return view('pokemons.index', compact('pokemons'));
     }
+
 
     /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-        //
+        return view('pokemons.create');
     }
+
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'form' => 'required|string|max:255',
+            'type1' => 'required|string|min:0',
+            'type2' => 'required|string|min:0',
+            'total' => 'required|integer|min:0',
+            'hp' => 'required|integer|min:0',
+            'attack' => 'required|integer|min:0',
+            'defense' => 'required|integer|min:0',
+            'sp-atk' => 'required|integer|min:0',
+            'sp-def' => 'required|integer|min:0',
+            'speed' => 'required|integer|min:0',
+            'generation' => 'required|integer|min:0',
+        ]);
+
+        Pokemon::create($request->all());
+        return redirect()->route('pokemons.index')->with('success', 'Pokémon creato con successo!');
     }
+
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $pokemon = Pokemon::findOrFail($id);
+        return view('pokemons.show', compact('pokemon'));
     }
+
 
     /**
      * Show the form for editing the specified resource.
--- app/Models/Pokemon.php
+++ app/Models/Pokemon.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Pokemon extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'form', 'type1', 'type2', 'total', 'hp', 'atttack', 'defense', 'sp-atk', 'sp-def', 'speed', 'generation'];
 }
